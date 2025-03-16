4c308c9f12013dcc78b2a3ca674a457de937f615
KaikSAlves <souzaalveskaik@gmail.com>
2025-03-16 17:48:25 -0300

    Configurado o nodemon e o servidor da api

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import adicionarRotas from './routes.js';
+
+const servidor = express();
+servidor.get(express.json());
+servidor.use(cors());
+
+adicionarRotas(servidor);
+
+const PORT = process.env.PORT;
+
+servidor.listen(PORT, () => console.log(`API subiu na porta ${PORT}`));
